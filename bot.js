d784247b9e4a1c702d56dbc4142005306df4c7d7
Will Taylor <taylorw@udel.edu>
2016-10-05 09:42:19 -0400

    Added more bars

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ var command;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\!happy hour .*/;
+      botRegex = /^\!happy hour (.)*/;
 
   if(request.text && botRegex.test(request.text)) {
     command = request.text;
